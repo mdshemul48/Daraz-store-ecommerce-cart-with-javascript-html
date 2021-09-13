a23bb5f09d57f5d1d7b9ed9b578f1530c7b50e79
MD Shimul <mdshemul480@gmail.com>
2021-09-13 11:21:55 +0600

    adding new bootstrap

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`;
+  const url = `https://raw.githubusercontent.com/ProgrammingHero1/ranga-store-api/main/ranga-api.json`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
